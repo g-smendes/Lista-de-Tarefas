0ee4d84c66d1e1c831f42e13f368d27a29ed6893
Gabriel Silva Mendes <gsmmendes58@gmail.com>
2023-07-20 21:49:14 -0300

    first commit

--- src/components/QuizComplet.tsx
+++ src/components/QuizComplet.tsx
@@ -33,29 +33,28 @@ export const QuizComplet = () => {
         <div className="w-full h-screen flex justify-center items-center bg-gray-600"> 
             <div className="w-full max-w-xl rounded-md bg-slate-100  text-black shadow shadow-black" >
               <div className="p-5 font-bold text-2xl border border-gray-300">{title}</div>
-              <div className="p-5">
+                <div className="p-5">
   
-                {!showResult &&
-                  <QuestionItem
-                    question={questions[currentQuestion]}
-                    count={currentQuestion + 1}
-                    onAnswer={handleAnswered}
-                  />
-                }
-  
-                {showResult &&
-                  <Results questions={questions} answers={answers} /> 
-                }
-  
-              </div>
-              <div className="p-5 text-center border-t border-gray-300">
-                {!showResult && 
-                  `${currentQuestion + 1} de ${questions.length} pergunta${questions.length === 1 ? '' : 's'}`
-                }
-                {!showResult && 
-                  <button onClick={handleRestartButton} className="px-3 py-2 rounded-md bg-blue-800 text-white">Reiniciar Quiz</button>
-                }
-              </div>
+                    {!showResult &&
+                    <QuestionItem
+                        question={questions[currentQuestion]}
+                        count={currentQuestion + 1}
+                        onAnswer={handleAnswered}
+                    />
+                    }
+    
+                    {showResult &&
+                    <Results questions={questions} answers={answers} /> 
+                    }
+                </div>
+                <div className="p-5 text-center border-t border-gray-300">
+                    {!showResult && 
+                    `${currentQuestion + 1} de ${questions.length} pergunta${questions.length === 1 ? '' : 's'}`
+                    }
+                    {!showResult && 
+                    <button onClick={handleRestartButton} className="px-3 py-2 rounded-md bg-blue-800 text-white">Reiniciar Quiz</button>
+                    }
+                </div>
             </div>
         </div>
     );
